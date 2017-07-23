1f2309b8989d0188e98729013dfa1ad51efdabe2
AMIT GIRI <mamitgiri@outlook.com>
2017-07-23 13:40:38 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/* 
+File Name   : hello.c
+Author      : AMIT M GIRI
+description : Hello world program  
+
+
+*/
+
+
+#include<stdio.h> //The stdio.h header file defines variable types, several macros, and various functions for performing input and output
+
+
+int main (void ) //  main function returns int value 
+{
+  printf("Hello, World !");
+  return 0;
+}
